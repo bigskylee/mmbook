3783e2cd3b3f8363b16beec363ba4c64d2b73d5b
bigskylee <sodlfmf@naver.com>
2024-07-10 18:39:05 +0900

    노래 테스트

--- index.js
+++ index.js
@@ -9,32 +9,36 @@ const fs = require('fs')
 var cheerio = require('cheerio')
 var request = require('request')
 // aspose.cells = require('aspose.cells')
+app.use(express.static('static'))
 
 /* GET home page. */
 app.get('/', async function (req, res, next) {
-    const html = path.resolve(__dirname + '/home.html')
+    const html = path.resolve(__dirname + '/static/html/home.html')
 
     res.sendFile(html)
 })
 
 app.use(express.urlencoded({ extended: false }))
-app.post('/test', async function (req, res, next) {
-    const surch = req.body
+app.get('/test', async function (req, res, next) {
+    const surch = req.query.test
     // res.send(surch)
+    console.log(surch)
     try {
         var number = 1
         var url =
             'https://www.tjmedia.com/tjsong/song_search_list.asp?strType=2&natType=&strText=' +
-            surch.test +
+            surch +
             '&strCond=0&intPage='
         //String(surch.page)
-        console.log(surch.page)
+        //console.log(surch.page)
         var song = []
         var pagechack = url + '1'
         const numpage = await axios.get(pagechack)
         var P = cheerio.load(numpage.data)
+        var numbers = 0
         P('#page1').each((i, row) => {
-            const numbers = P(row).find('a') + 1
+            numbers = P(row).find('a').length
+            console.log(numbers)
         })
         var url2 = url + surch.page
         // while (chack == 1) {
@@ -44,24 +48,23 @@ app.post('/test', async function (req, res, next) {
         $('#BoardType1').each((i, row) => {
             const musictrs = $(row).find('tr')
             // 예를 들어 첫 번째 td 요소만 가져오기
-            console.log(musictrs[1])
             var t = 1
             while (t <= 15) {
                 wtexttr = $(musictrs[t]).find('td')
                 mnumber = $(wtexttr[0]).text()
                 mname = $(wtexttr[1]).text()
-                msinga = $(wtexttr[2]).text()
+                msinger = $(wtexttr[2]).text()
                 mcomposer = $(wtexttr[3]).text()
                 mlyricist = $(wtexttr[4]).text()
                 if (mnumber == '') {
                     console.log('stop')
                     break
                 }
-                console.log(mnumber, mname, msinga, mcomposer, mlyricist)
+                console.log(mnumber, mname, msinger, mcomposer, mlyricist)
                 song.push({
                     mnumber: mnumber,
                     mname: mname,
-                    msinga: msinga,
+                    msinger: msinger,
                     mcomposer: mcomposer,
                     mlyricist: mlyricist,
                 })
@@ -83,22 +86,29 @@ app.post('/test', async function (req, res, next) {
                     song[index]['mname'] +
                     '</td>' +
                     '<td>' +
-                    song[index]['msinga'] +
+                    song[index]['msinger'] +
                     '</td>' +
-                    '<td>' +
+                    '<td class="tablet">' +
                     song[index]['mcomposer'] +
                     '</td>' +
-                    '<td>' +
+                    '<td class="tablet">' +
                     song[index]['mlyricist'] +
                     '</td>' +
                     '</tr>',
             )
         }
-        let result = txts.join('')
-        const fs = require('fs')
 
-        const ahtml = fs.readFileSync(__dirname + '/test.html', 'utf-8')
-        const shtml = ahtml.replace('<testsong />', '<div>' + result + '</div>')
+        var pages = []
+        for (let index = 1; index < numbers + 2; index++) {
+            pages.push('<a href="test?test=' + surch + '&page=' + index + '">[' + index + ']</a>')
+        }
+
+        let result = txts.join('')
+        let page = pages.join('')
+        const fs = require('fs')
+        const ahtml = fs.readFileSync(__dirname + '/static/html/test.html', 'utf-8')
+        const ahtml2 = ahtml.replace('<testsong />', '<div>' + result + '</div>')
+        const shtml = ahtml2.replace('<pagelink />', '<div>' + page + '</div>')
 
         res.send(shtml)
     } catch (e) {
