fedb593bb08b28f77caee22f0a4783501896d7af
bigskylee <sodlfmf@naver.com>
2024-08-08 10:25:10 +0900

    로그인 회원가입 완료

--- static/js/logout.js
+++ static/js/logout.js
@@ -1,8 +1,15 @@
 document.getElementById('logout').addEventListener('click', function () {
-    const form = document.createElement('form')
-    form.setAttribute('method', 'post')
-    form.setAttribute('action', `/logout`)
+    if (confirm('정말 로그아웃합니까?') == true) {
+        //확인
+        // document.form.submit()
+        const form = document.createElement('form')
+        form.setAttribute('method', 'post')
+        form.setAttribute('action', `/logout`)
 
-    document.body.appendChild(form)
-    form.submit()
+        document.body.appendChild(form)
+        form.submit()
+    } else {
+        //취소
+        return
+    }
 })
